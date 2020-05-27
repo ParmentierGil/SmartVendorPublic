8d91919fcaf91d0341487c2a584b6877155a6a59
ParmentierGil <gil.parmentier@student.howest.be>
2020-05-27 11:11:11 +0200

    LCD library

--- Code/Backend/helpers/Keypad.py
+++ Code/Backend/helpers/Keypad.py
@@ -16,19 +16,26 @@ COL_PINS = [5, 11, 9, 10]
 factory = rpi_gpio.KeypadFactory()
 
 class Keypad(threading.Thread):
-    def __init__(self, message_queue):
+    def __init__(self, message_queue, lcd):
         threading.Thread.__init__(self)
         self.message_queue = message_queue
         self.keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
         self.input_string = ""
+        self.lcd = lcd
 
     def handle_key_press(self, key):
         if key == 'A':
             product_name = DataRepository.get_product_by_number(int(self.input_string))
-            print(product_name)
             self.input_string = ""
+
+            if product_name == None:
+                print('Geen product met dit nummer.')
+            else:
+                print(product_name['Name'])
         elif key in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
             self.input_string += key
+            print(self.input_string)
+            self.lcd.write_message(self.input_string)
 
     def run(self):
         try:
